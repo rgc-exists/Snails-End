59c0af2ac32362853b30afa2bf51e3868a00217e
rgc-exists <103580754+rgc-exists@users.noreply.github.com>
2025-07-05 14:52:02 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Snail's End
+
+The offiicial hardest levels list for Will You Snail! Inspired by the Geometry Dash demon list.
+
+# THIS IS A WORK-IN-PROGRESS! View the existing website HERE: https://sites.google.com/view/snailsend/list
+
+# The website (WIP)
+
+(Website link goes here)
---
+++ js/list.js
@@ -0,0 +1,131 @@
+const d = {
+  list: [
+    /*=================================================================================*/
+    {
+      vids: [
+        {
+          user: "Rosalinor",
+          link: "https://youtu.be/QPjioKGG3rk",
+        },
+      ],
+      name: "Extinction",
+      author: "ShadyIsCool [x8c8r]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3036063270",
+      verificationVid: "https://youtu.be/iZWlhx9VZYw?si=QGYOJfNzDB75yxec",
+      quote: "This will last at least 4 days!",
+      campaign: "Standalone Level",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [],
+      name: "The End Of Buff",
+      author: "adpolazyz [Rosalinor]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3033307693",
+      verificationVid: "https://youtu.be/iN6kROY2tRs?si=NNe_t30RSumtqrVZ",
+      quote: "Should be buffed",
+      campaign: "The Buffing Station",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [
+        {
+          user: "Rosalinor",
+          link: "https://www.youtube.com/watch?v=EPe7rZtfRcI",
+        },
+      ],
+      name: "Trench",
+      author: "adpolazyz [x8c8r]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3172623897",
+      verificationVid: "https://www.youtube.com/watch?v=OnLccB_wmxQ",
+      quote: "Dedicated to my arch-rival rosalinor <3",
+      campaign: "Standalone Level",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [
+        {
+          user: "x8c8r",
+          link: "https://youtu.be/s1Gt2vZwJtw",
+        },
+      ],
+      name: "A13",
+      author: "x8c8r [Rosalinor]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3018420799",
+      verificationVid: "https://www.youtube.com/watch?v=_WqZ3nLSCso",
+      quote: "Literally just A13, there is nothing else",
+      campaign: "Xeight's Awesome Campaig",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [
+        {
+          user: "x8c8r",
+          link: "https://youtu.be/1ukXPLYHDOE",
+        },
+        {
+          user: "masrock9",
+          link: "https://youtu.be/9JthCn5gAj4",
+        },
+      ],
+      name: "Timeless",
+      author: "adpolazyz [Rosalinor]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3035140442",
+      verificationVid: "https://www.youtube.com/watch?v=_uitDG1pQ2s",
+      quote: "It's time to kick bubblegum and chew ass",
+      campaign: "Standalone Level",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [],
+      name: "Simpl!city",
+      author: "adpolazyz [Rosalinor]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3035916768",
+      verificationVid: "https://www.youtube.com/watch?v=_uitDG1pQ2s",
+      quote: "Not so simple after all...",
+      campaign: "Standalone Level",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [
+        {
+          user: "BrokenCalculator14",
+          link: "https://youtu.be/ZKf9sv6TpxQ",
+        },
+      ],
+      name: "Puzzle of Pain",
+      author: "mischiefmarc [Rosalinor]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3034033387",
+      verificationVid: "https://www.youtube.com/watch?v=_v5Efu6iFv4",
+      quote: "The puzzle of DOOM!",
+      campaign: "Standalone Level",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [
+        {
+          user: "kgamesnyc",
+          link: "https://youtu.be/5I6WBSCMU8E",
+        },
+      ],
+      name: "Eclipse",
+      author: "adpolazyz [Rosalinor]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3037072433",
+      verificationVid: "https://www.youtube.com/watch?v=HyltmxE0xeA",
+      quote: "Claustrophobia is your best friend",
+      campaign: "Standalone Level",
+    },
+    /*=================================================================================*/
+  ],
+  /*only modify version with every MAJOR update (you can always check update number at https://github.com/electroflameofficial/gdshittylist/releases*/
+  version: [1, 0, 0],
+};
+
+const list = d.list;
+const version = d.version;
