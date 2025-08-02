ca06fed43f9a616a7bd174219199f64b25cf61fc
rgc-exists <103580754+rgc-exists@users.noreply.github.com>
2025-08-02 13:07:36 -0400

    Finish transferring Google Sites database

--- js/list.js
+++ js/list.js
@@ -113,6 +113,10 @@ const d = {
           user: "kgamesnyc",
           link: "https://youtu.be/5I6WBSCMU8E",
         },
+        {
+          user: "playercore",
+          link: "https://youtu.be/N1vHZz4fVY8",
+        },
       ],
       name: "Eclipse",
       author: "adpolazyz [Rosalinor]",
@@ -120,6 +124,266 @@ const d = {
       verificationVid: "https://www.youtube.com/watch?v=HyltmxE0xeA",
       quote: "Claustrophobia is your best friend",
       campaign: "Standalone Level",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [
+        {
+          user: "x8c8r",
+          link: "https://youtu.be/k0v6Hcx5bbI",
+        },
+        {
+          user: "BrokenCalculator14",
+          link: "https://youtu.be/LtrvavmGC7g",
+        },
+        {
+          user: "mischiefmarc",
+          link: "https://youtube.com/clip/UgkxYfKOiajj15navaopBIn2ig_UiR4WI6Yw?si=r6WqH1p3iYEApA3r",
+        },
+        {
+          user: "probablyicicle",
+          link: "https://youtu.be/2PyZF7x0bVo",
+        },
+        {
+          user: "Rosalinor",
+          link: "https://youtu.be/yMDs9CpXYrk",
+        },
+      ],
+      name: "Blubble",
+      author: "mischiefmarc [mischiefmarc]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3032254845",
+      verificationVid: "https://youtu.be/32IorIUfQl4",
+      quote: "Horray",
+      campaign: "Standalone Level",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [
+        {
+          user: "MacaroniAndGames",
+          link: "https://youtu.be/qGjSapsHqVc",
+        },
+        {
+          user: "PlayerCore",
+          link: "https://www.youtube.com/watch?v=uHrchvcLYGs",
+        },
+      ],
+      name: "Corner Madness",
+      author: "adpolazyz [Rosalinor]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3031814119",
+      verificationVid: "https://www.youtube.com/watch?v=GeqwKPsQTzc",
+      quote: "Cornered the corner because the cornered corner is cornering",
+      campaign: "Path Of Sufferment",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [],
+      name: "The Vortex",
+      author: "adpolazyz [Rosalinor]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3034778307",
+      verificationVid: "https://www.youtube.com/watch?v=GCuS7z9lL2c",
+      quote: "A very fun underwater level that will test your skills!",
+      campaign: "Standalone Level",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [
+        /*
+        // RECORDS FROM THE OLD VERSION OF THE LEVEL
+        {
+          user: "x8c8r",
+          link: "https://youtu.be/k0v6Hcx5bbI",
+        },
+        {
+          user: "BrokenCalculator14",
+          link: "https://youtu.be/LtrvavmGC7g",
+        },
+        {
+          user: "mischiefmarc",
+          link: "https://youtube.com/clip/UgkxYfKOiajj15navaopBIn2ig_UiR4WI6Yw?si=r6WqH1p3iYEApA3r",
+        },
+        {
+          user: "probablyicicle",
+          link: "https://youtu.be/2PyZF7x0bVo",
+        },
+        // RECORDS FROM THE OLD VERSION OF THE LEVEL
+        */
+
+        {
+          user: "Rosalinor",
+          link: "https://youtu.be/yMDs9CpXYrk",
+        },
+      ],
+      name: "Stage Five",
+      author: "ppds [RGC_Exists]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3032254845",
+      verificationVid: "https://www.youtube.com/watch?v=dp8OO0VQDsA",
+      // OLD VERSION VERIFICATION: https://youtu.be/lpyyk9hZfAM
+      quote: "Stage the fifth of Gibraltar",
+      wasUpdated: true,
+      campaign: "Speed Racer",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [],
+      name: "HAPPY HAPPY HAPPY HAPPY HAPPY",
+      author: "adpolazyz [Rosalinor]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3031814119",
+      verificationVid: "https://www.youtube.com/watch?v=GFEYucdveaU",
+      quote: "Happy happy happy",
+      campaign: "Path Of Sufferment",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [
+        /*
+        // RECORDS FROM THE OLD VERSION OF THE LEVEL
+        {
+          user: "x8c8r",
+          link: "https://youtu.be/uFbtqac68HQ",
+        },
+        // RECORDS FROM THE OLD VERSION OF THE LEVEL
+        */
+      ],
+      name: "Trans Rights",
+      author: "kgamesnyc",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3257979776",
+      verificationVid: "https://www.youtube.com/watch?v=U4Swq4IrJaY",
+      quote: ":3",
+      wasUpdated: true,
+      campaign: "Standalone Level",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [
+        {
+          user: "Rosalinor",
+          link: "https://youtu.be/cgwGlaxDg_0",
+        },
+        {
+          user: "BootifulBlaze",
+          link: "https://youtu.be/A3z_Ilg7a70",
+        },
+      ],
+      name: "Stage Four",
+      author: "ppds",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3032254845",
+      verificationVid: "https://www.youtube.com/watch?v=ItZm9RZq27g",
+      quote: "CLUBSTEP MONSTER OMFG",
+      campaign: "Speed Racer",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [],
+      name: "Shutter Bug",
+      author: "BootifulBlaze",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3053296300",
+      verificationVid: "https://www.youtube.com/watch?v=Ig3kUVUdyTU",
+      quote:
+        "You just got RGC Exist'd. Send this to your friends to totally RGC Exist them ( ͡° ͜ʖ ͡°)",
+      campaign: "Standalone Level",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [],
+      name: "Pain",
+      author: "x8c8r",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3018420799",
+      verificationVid: "https://www.youtube.com/watch?v=pCqZQv7Np0I",
+      quote: "Pain",
+      campaign: "xeight's awesome campaig",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [
+        {
+          user: "NikoHatesTaxes",
+          link: "https://youtu.be/Er2OmoQCKlY",
+        },
+      ],
+      name: "Unnerfed A06",
+      author: "pixxeell___ [rosalinor]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3058113716",
+      verificationVid: "https://www.youtube.com/watch?v=sQAXoZ7x1hw",
+      quote: "This is too short for real",
+      campaign: "Standalone Level",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [
+        {
+          user: "MacaroniAndGames",
+          link: "https://www.youtube.com/watch?v=YIaMy-g3fdQ",
+        },
+      ],
+      name: "More of Pain",
+      author: "adpolazyz [BrokenCalculator14]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3031814119",
+      verificationVid: "https://www.youtube.com/watch?v=YIaMy-g3fdQ",
+      quote: "Painfully similar to start of sufferment",
+      campaign: "Path Of Sufferment",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [],
+      name: "Lucky Run",
+      author: "BootifulBlaze",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3052539549",
+      verificationVid: "https://www.youtube.com/watch?v=KwGO_LX4Ueg",
+      quote: "Luck, Timings, and Speedrunning all in one",
+      campaign: "Standalone Level",
+    },
+    /*=================================================================================*/
+    /*=================================================================================*/
+    {
+      vids: [
+        {
+          user: "x8c8r",
+          link: "https://youtu.be/de9rzErAC08",
+        },
+        {
+          user: "mischiefmarc",
+          link: "https://youtube.com/clip/Ugkx3b6s8127u1VQn3ZJJfyzRu5d9w_Oh84L?si=yJyWLWsgUr8kL6AS",
+        },
+        {
+          user: "probablyicicle",
+          link: "https://youtu.be/k1JRxgSgR_Y",
+        },
+        {
+          user: "BootifulBlaze",
+          link: "https://youtu.be/XXu7z79iO5g",
+        },
+        {
+          user: "Rosalinor",
+          link: "https://youtu.be/XrgRbixyA_E",
+        },
+        {
+          user: "adpolazyz",
+          link: "https://youtu.be/aLeOAo7vNsU",
+        },
+        {
+          user: "MacaroniAndGames",
+          link: "https://youtu.be/obKw0vRQmZc",
+        },
+      ],
+      name: "The Start of Suffering",
+      author: "adpolazyz [BrokenCalculator14]",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3031814119",
+      verificationVid: "https://www.youtube.com/watch?v=YIaMy-g3fdQ",
+      quote: "Jonas played this lolol",
+      campaign: "Path Of Sufferment",
     },
     /*=================================================================================*/
   ],
